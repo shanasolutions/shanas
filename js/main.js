5bb4ee9ee09ae9d497cb62c187bb7f4e9b2c409a
shanasolutions <66355341+shanasolutions@users.noreply.github.com>
2022-03-06 18:20:23 +0530

    changed code

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-const myslide = document.querySelectorAll('.myslider'),
-    dot = document.querySelectorAll('.dot');
-    
-    let counter = 1;
-    slidefun(counter);
-    
-    let timer = setInterval(autoslide, 8000);
-    function autoslide(){
-        counter += 1;
-        slidefun(counter);
-    }
-    function plusSlides(n){
-        counter +=n;
-        slidefun(counter);
-        resetTimer();
-    }
-    function currentSlide(n){
-        counter = n;
-        slidefun(counter);
-        resetTimer();
-    }
-    function resetTimer(){
-        clearInterval(timer);
-        timer = setInterval(autoslide, 8000);
-    }
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
 
-    function slidefun(n){
-        let i;
-        for(i = 0;i<myslide.length;i++){
-            myslide[i].style.dislpay = "none";
-        }
-        for(i = 0;i<dot.length;i++){
-            dot[i].classList.remove('active');
-        }
-        if(n > myslide.length){
-            counter = 1;
-        }
-        if(n < 1){
-            counter = myslide.length;
-        }
-        myslide[counter - 1].style.dislpay = "block";
-        dot[counter - 1].classList.add('active');
-    }
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
